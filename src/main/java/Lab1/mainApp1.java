a1e34b331a4776001cb94657a76ca32356eb6f08
tykongab <143928825+tykongab@users.noreply.github.com>
2023-09-03 19:50:43 +0800

    Add program started message

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -8,6 +8,7 @@ public class mainApp1 {
         int b = 2;
         int n = 11;
         System.out.println("Welcome to Scientific Calculator!");
+        System.out.println("Program started ...");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = "+ myLibrary.factorial(n));
         System.out.println("Program ..Ended ...");
